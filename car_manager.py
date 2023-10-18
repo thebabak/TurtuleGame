61b63c4021537e6e276857ec075e1a420af267e0
Babak <babak.bayanian@gmail.com>
2023-10-18 15:06:43 +0330

    first edit

---
+++ car_manager.py
@@ -0,0 +1,20 @@
+from turtle import Turtle
+import random
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager:
+    def __init__(self):
+        self.all_car=[]
+        
+    def create_car(self):
+        new_car=Turtle("square")
+        new_car.shapesize(stretch_len=2,stretch_wid=1)
+        new_car.penup()
+        new_car.color(random.choice(COLORS))
+        random_y=random.randint(-250,250)
+        new_car.goto(300,random_y)
+        self.all_car.append(new_car)
+        
